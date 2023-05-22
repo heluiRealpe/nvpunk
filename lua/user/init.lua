559ca9ad26c4827a4f94eee047dba86dbc8dca83
heluiRealpe <hrealpe@gmail.com>
2023-05-22 12:32:11 +0200

    dap commented

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -21,29 +21,29 @@ vim.keymap.set('v', '<leader>p', '"+p', keymap_opts)
 vim.keymap.set('v', '<leader>p', '"+P', keymap_opts)
 
 
-local dap = require'dap'
-
-dap.adapters.node2 = {
-    type = 'executable',
-    command = 'node',
-    args = { '~/.local/share/nvim/mason/packages/node-debug2-adapter/out/src/nodeDebug.js' }
-}
-dap.configurations.javascript = {
-  {
-    name = 'Launch',
-    type = 'node2',
-    request = 'launch',
-    program = '${file}',
-    cwd = vim.fn.getcwd(),
-    sourceMaps = true,
-    protocol = 'inspector',
-    console = 'integratedTerminal',
-  },
-  {
-    -- For this to work you need to make sure the node process is started with the `--inspect` flag.
-    name = 'Attach to process',
-    type = 'node2',
-    request = 'attach',
-    processId = require'dap.utils'.pick_process,
-  },
-}
+-- local dap = require'dap'
+--
+-- dap.adapters.node2 = {
+--     type = 'executable',
+--     command = 'node',
+--     args = { '~/.local/share/nvim/mason/packages/node-debug2-adapter/out/src/nodeDebug.js' }
+-- }
+-- dap.configurations.javascript = {
+--   {
+--     name = 'Launch',
+--     type = 'node2',
+--     request = 'launch',
+--     program = '${file}',
+--     cwd = vim.fn.getcwd(),
+--     sourceMaps = true,
+--     protocol = 'inspector',
+--     console = 'integratedTerminal',
+--   },
+--   {
+--     -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+--     name = 'Attach to process',
+--     type = 'node2',
+--     request = 'attach',
+--     processId = require'dap.utils'.pick_process,
+--   },
+-- }
